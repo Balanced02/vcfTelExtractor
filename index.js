d7aca45a2d9a8dc4c7cdcb256aad70dab94fd089
Ayomide Daniel <adebalanced02@gmail.com>
2018-06-12 09:17:09 +0100

    updated error message on no file path

--- index.js
+++ index.js
@@ -1,20 +1,18 @@
-var readFile = require('./readFile')
+var readFile = require("./readFile");
 
-function extractTel(path, callback, {
-    prefix = false
-} = {}) {
-    if (!path) callback('Please provide a path')
-    readFile(path, (err, data) => {
-        if (err) {
-            callback(err)
-            return
-        }
-        let newData = data.replace(/-|\s/g, ' ')
-        let re = /([0-9]{9})\w*/g
-        if (prefix) re = /(\+?[0-9]{9})\w*/g
-        let matches = newData.match(re)
-        callback(null, matches)
-    })
+function extractTel(path, callback, { prefix = false } = {}) {
+  if (!path) callback(new Error("Please provide a path"));
+  readFile(path, (err, data) => {
+    if (err) {
+      callback(err);
+      return;
+    }
+    let newData = data.replace(/-|\s/g, " ");
+    let re = /([0-9]{9})\w*/g;
+    if (prefix) re = /(\+?[0-9]{9})\w*/g;
+    let matches = newData.match(re);
+    callback(null, matches);
+  });
 }
 
-module.exports = extractTel
+module.exports = extractTel;
